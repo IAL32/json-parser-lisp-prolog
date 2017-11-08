d2999e2a5cb4a2740d18c3050147c32a514d1679
Adrian <adrian.d.castro.t@gmail.com>
2017-11-08 11:47:41 +0100

    Aggiornato

--- 746215_Leonetti_Alessandro_LP_E2P_JSON_2015/Lisp/README.txt
+++
@@ -1,5 +0,0 @@
-Componenti del gruppo:
-
-- 746215 Leonetti Alessandro
-- 736424 Cassataro Riccardo
-- 728590 Trevi Alessandro
--- p.pl
+++ p.pl
@@ -4,83 +4,121 @@ r :- [p].
 t :- trace.
 %% debug only: sbatti scrivere nodebug ogni volta per uscire dal debug(mi esce sia dal trace che dal debug)
 nd :- nodebug.
-%% invece di dover usare ` ogni volta
-:- set_prolog_flag(double_quotes, chars).
+%% debug only: invece di dover usare ` ogni volta
+%:- set_prolog_flag(double_quotes, chars).
+
+% json_load/2
+json_load(FileName, JSON) :-
+    open(FileName, read, BufferIn),
+    read_string(BufferIn, _, String),
+    atom_chars(String, Chars),
+    json_parse(Chars, JSON),
+    close(BufferIn).
+
+% json_write/2
+% TODO
+json_write(JSON, FileName) :-
+    json_parse(JSON, String),
+    open(FileName, write, BufferOut),
+    write(BufferOut, String),
+    close(BufferOut).
 
 json_parse(JSONString, Object) :-
-    json_object(Object, JSONString, []).
+    json_object(Object, JSONString, []), !.
 json_parse(JSONString, Array) :-
-    json_array(Array, JSONString, []).
+    json_array(Array, JSONString, []), !.
 
 % json_array/3
 json_array(json_array([])) -->
-    "[", "]".
+    "[", ws, "]", ws.
 json_array(jsonarray(Array)) -->
-    "[", json_array_members(Array), "]".
+    "[", ws, json_array_members(Array), ws, "]", ws.
 
 % json_object/3
 json_object(jsonobject([])) -->
-    "{", "}".
+    "{", ws, "}".
 json_object(jsonobject(Object)) -->
-    "{", json_members(Object), "}".
+    "{", ws, json_members(Object), ws, "}", ws.
 
 % json_array_members/3
 json_array_members([Member | Members]) -->
-    json_value(Member),
-    ",",
+    json_value(Member), ws,
+    ",", ws,
     json_array_members(Members).
 json_array_members([Member]) -->
-    json_value(Member).
+    json_value(Member), !.
 
 %json_members
-json_members([Member | Members]) -->
-    json_pair(Member),
-    ",", !,
-    json_members(Members).
 json_members([Object]) -->
     json_pair(Object).
+json_members([Member | Members]) -->
+    json_pair(Member), ws,
+    ",", ws,
+    json_members(Members).
 
 % json_pair/3
 json_pair((Key, Value)) -->
-    json_key(Key), ":", json_value(Value).
+    json_key(Key), ws, ":", ws, json_value(Value).
 
 % json_key/3
 json_key(Key) -->
-    "\"", json_key_string(Chars), { atom_chars(Key, Chars) }, "\"".
-
-% json_value/3
-json_value(Value) -->
-    json_values(Value).
+    ("\"", json_key_string(Chars), { atom_chars(Key, Chars) }, "\"")
+    |
+    ("'", json_key_string(Chars), { atom_chars(Key, Chars) }, "'")
+    |
+    ("`", json_key_string(Chars), { atom_chars(Key, Chars) }, "`").
 
 % json_values/3
-json_values(Value) -->
-    "\"",
-    json_value_string(Codes),
-    { atom_chars(Value, Codes) },
-    "\"".
-json_values(Value) -->
+json_value(Value) -->
+    (("\"", "\"")
+    |
+    ("`", "`")),
+    { atom_chars(Value, "") }.
+json_value(Value) -->
+    ("\"", !, json_value_string_q(Codes), { atom_chars(Value, Codes) }, "\"")
+    |
+    ("`", !, json_value_string_a(Codes), { atom_chars(Value, Codes) }, "`"), !.
+json_value(Value) -->
     json_object(Value), !.
-json_values(Value) -->
+json_value(Value) -->
     json_array(Value), !.
-json_values(Value) -->
-    json_value_double(Codes),
-    { number_chars(Value, Codes) }, !.
-json_values(Value) -->
-    json_value_integer(Codes),
+json_value(Value) -->
+    json_value_number(Number), % analizzo la prima parte del numero,
+    ".",
+    json_value_number(Mantissa), % analizzo la mantissa
+    { flatten([Number, "." | Mantissa], Float) }, % unisco le due liste e ne faccio una sola senza nested lists
+    % converto la lista prima in stringa, e poi in un atomo prolog
+    { string_to_list(Value1, Float), number_chars(Value, Value1) }, !.
+json_value(Value) -->
+    % un integer
+    json_value_number(Codes),
     { number_chars(Value, Codes) }, !.
 
 % restringo solamente ai caratteri alfanumerici
-json_key_string([]) --> [].
 json_key_string([H | T]) --> [H], { char_type(H, alnum) }, json_key_string(T).
+json_key_string([]) --> [].
 
-json_value_double(_Double) --> [].
-json_value_double(Double) --> [D], { char_type(D, digit) }, json_value_double(Double).
-json_value_integer(_Integer) --> [].
-json_value_integer(Integer) --> [I], { char_type(I, digit) }, json_value_integer(Integer).
-json_value_string([]) --> [].
-json_value_string([H | T]) --> [H], json_value_string(T).
+json_value_number([H | T]) --> [H], { char_type(H, digit) }, json_value_number(T).
+json_value_number([]) --> [].
+
+% json_value_string_q/2
+% una stringa json inclusa tra ", che non contiene "
+% TODO: effettivamente far sì che fallisca quando contiene un "
+json_value_string_q([H | T]) --> [H], { char_type(H, alnum) }, json_value_string_q(T).
+json_value_string_q([]) --> [].
+
+% json_value_string_a/2
+% una stringa json inclusa tra `, che non contiene `
+% TODO: effettivamente far sì che fallisca quando contiene un `
+json_value_string_a([H | T]) --> [H], { char_type(H, alnum) | char_type(H, space) }, json_value_string_a(T).
+json_value_string_a([]) --> [].
+
 
 % ws/2
 % consuma gli spazi bianchi
-ws --> [W], { char_type(W, space) }, ws.
+ws --> [W], { char_type(W, space) }, ws, !.
 ws --> [].
+
+
+
+%% getter e setter
