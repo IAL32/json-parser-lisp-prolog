fffca07f711fb329a71f5f930543f9536cf20186
Adrian <adrian.d.castro.t@gmail.com>
2017-11-09 23:32:09 +0100

    Boh ho riaggiustato tutto

---
+++ main.pl
@@ -0,0 +1,7 @@
+:- [settings].
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%      INIZIO DEL PROGRAMMA                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- [io].                % Input/Output
+:- [json_decode].       % Json Decode
+:- [json_encode].       % Json Encode
