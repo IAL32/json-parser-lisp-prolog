2c4ba34ea77221c3c7d8d45bf67267f0c9b34844
Adrian <adrian.d.castro.t@gmail.com>
2017-11-10 21:28:09 +0100

    Aggiornati commenti di json_encode

--- json_encode.pl
+++ json_encode.pl
@@ -1,4 +1,4 @@
-% json_encode/2
+% json_encode(+JsonObject, -String).
 json_encode(jsonobject([]), '{}').
 json_encode(jsonarray([]), '[]').
 
@@ -10,7 +10,7 @@ json_encode(jsonarray(Members), StringArray) :-
     json_encode_array_members(Members, StringMembers),
     atomic_list_concat(['[', StringMembers, ']'], StringArray).
 
-% json_encode_array_members/2
+% json_encode_array_members(+JsonArrayMembers, -StringMerged).
 json_encode_array_members([Member | Members], StringMerged) :-
     json_encode_value(Member, StringValue),
     json_encode_array_members(Members, StringMembers),
@@ -20,7 +20,7 @@ json_encode_array_members([Member | Members], StringMerged) :-
 json_encode_array_members([Member], StringMerged) :-
     json_encode_value(Member, StringMerged), !.
 
-% json_encode_members/2
+% json_encode_members(+JsonObjectMembers, -StringMerged).
 json_encode_object_members([Member | Members], StringMerged) :-
     json_encode_pair(Member, StringPair),
     json_encode_object_members(Members, StringMembers),
@@ -30,18 +30,18 @@ json_encode_object_members([Member | Members], StringMerged) :-
 json_encode_object_members([Member], StringMerged) :-
     json_encode_pair(Member, StringMerged), !.
 
-% json_encode_pair/2
+% json_encode_pair(+JsonPair, -StringPair).
 json_encode_pair((Key, Value), StringPair) :-
     atomic_list_concat(['"', Key, '"'], StringKey),
     json_encode_value(Value, StringValue),
     string_concat(StringKey, ':', StringKeyColon),
     string_concat(StringKeyColon, StringValue, StringPair).
 
-% json_encode_key/2
+% json_encode_key(+JsonPairKey, -StringKey).
 json_encode_key(Key, StringKey) :-
     json_encode_string(Key, StringKey).
 
-% json_encode_value/2
+% json_encode_value(+JsonValue, -StringValue).
 json_encode_value(Object, StringValue) :-
     json_encode(Object, StringValue),
     !.
@@ -55,6 +55,6 @@ json_encode_value(String, StringValue) :-
     json_encode_string(String, StringValue),
     !.
 
-% json_encode_string/2
+% json_encode_string(+JsonString, -StringValue).
 json_encode_string(String, StringValue) :-
     atomic_list_concat(['"', String, '"'], StringValue).
