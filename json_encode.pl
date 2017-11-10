ba8c519d718eac53d7f199bc28a9324a85084095
Adrian <adrian.d.castro.t@gmail.com>
2017-11-10 14:44:11 +0100

    Finito json_encodegit add .git add . Finita la parte di Prolog del progetto :D

--- json_encode.pl
+++ json_encode.pl
@@ -1,16 +1,58 @@
-json_encode(jsonobject([]), "{}").
-json_encode(jsonarray([]), "[]").
+% json_encode/2
+json_encode(jsonobject([]), '{}').
+json_encode(jsonarray([]), '[]').
 
-json_encode(jsonobject(Members), [StringMembers | String]) :-
-    json_encode_members(Members, StringMembers).
+json_encode(jsonobject(Members), StringObject) :-
+    json_encode_object_members(Members, StringMembers),
+    atomic_list_concat(['{', StringMembers , '}'], StringObject).
 
-json_encode_members([Member | Members], StringMerged) :-
+json_encode(jsonarray(Members), StringArray) :-
+    json_encode_array_members(Members, StringMembers),
+    atomic_list_concat(['[', StringMembers, ']'], StringArray).
+
+% json_encode_array_members/2
+json_encode_array_members([Member | Members], StringMerged) :-
+    json_encode_value(Member, StringValue),
+    json_encode_array_members(Members, StringMembers),
+    string_concat(StringValue, ',', StringValueComma),
+    string_concat(StringValueComma, StringMembers, StringMerged), !.
+
+json_encode_array_members([Member], StringMerged) :-
+    json_encode_value(Member, StringMerged), !.
+
+% json_encode_members/2
+json_encode_object_members([Member | Members], StringMerged) :-
     json_encode_pair(Member, StringPair),
-    json_encode_members(Members, StringMembers),
-    string_concat(StringPair, ",", StringPairComma),
-    string_concat(StringPairComma, StringMembers, StringMerged).
+    json_encode_object_members(Members, StringMembers),
+    string_concat(StringPair, ',', StringPairComma),
+    string_concat(StringPairComma, StringMembers, StringMerged), !.
 
+json_encode_object_members([Member], StringMerged) :-
+    json_encode_pair(Member, StringMerged), !.
+
+% json_encode_pair/2
 json_encode_pair((Key, Value), StringPair) :-
-    atomic_list_concat(["\"", Key, "\""], StringKey),
+    atomic_list_concat(['"', Key, '"'], StringKey),
     json_encode_value(Value, StringValue),
-    string_concat(StringKey, ":", String).
+    string_concat(StringKey, ':', StringKeyColon),
+    string_concat(StringKeyColon, StringValue, StringPair).
+
+% json_encode_key/2
+json_encode_key(Key, StringKey) :-
+    json_encode_string(Key, StringKey).
+
+% json_encode_value/2
+json_encode_value(Object, StringValue) :-
+    json_encode(Object, StringValue), !.
+json_encode_value(Number, StringNumber) :-
+    number(Number),
+    atom_number(AtomNumber, Number),
+    atom_chars(AtomNumber, NumberChars),
+    string_chars(StringNumber, NumberChars),
+    !.
+json_encode_value(String, StringValue) :-
+    json_encode_string(String, StringValue), !.
+
+% json_encode_string/2
+json_encode_string(String, StringValue) :-
+    atomic_list_concat(['"', String, '"'], StringValue).
